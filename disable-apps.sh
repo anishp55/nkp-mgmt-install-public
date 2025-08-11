fc6358b90f1ee5f83646e50a5fdd0c781b43f66b
akp <anish.patel@nutanix.com>
2025-08-11 14:46:57 -0400

    added missing $

--- disable-apps.sh
+++ disable-apps.sh
@@ -4,5 +4,5 @@ set +e
 
 for i in $(cat app-list | grep disable)
 do
-   kubectl delete appdeployment -n kommander  (echo $i | awk -F"," '{print $1}')
+   kubectl delete appdeployment -n kommander  $(echo $i | awk -F"," '{print $1}')
 done
