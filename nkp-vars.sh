00ca41eb72054fd4687cc15323d29abcd4824566
anishp55 <anishs.online.bills@gmail.com>
2026-01-20 11:36:48 -0500

    Update environment variables in nkp-vars.sh
    Updated various environment variables for new IP addresses and paths.

--- nkp-vars.sh
+++ nkp-vars.sh
@@ -14,7 +14,7 @@ export CLUSTER_NAME=rccl-example
 # Keep the password enclosed between single quotes - Ex: 'password'
 #set as env var through GHA secrets
 # Prism Central IP address - Ex: 10.38.30.7
-export NUTANIX_ENDPOINT=10.42.73.7
+export NUTANIX_ENDPOINT=10.8.50.7
 
 # Prism Central port (default: 9440)
 # Do not change it
@@ -22,11 +22,11 @@ export NUTANIX_PORT=9440
 
 # Kubernetes VIP. Must be in the same subnet as the VMs
 # Check the table to find your IP
-export CONTROL_PLANE_ENDPOINT_IP=10.42.73.140
+export CONTROL_PLANE_ENDPOINT_IP=10.8.50.140
 
 # Load balancer IP range. Format: <first_ip>-<last_ip>
 # Check the table to find your IP
-export LB_IP_RANGE=10.42.73.141-10.42.73.144
+export LB_IP_RANGE=10.8.50.141-10.42.73.144
 
 # NKP Rocky image name
 # Do not change it
@@ -37,7 +37,7 @@ export NUTANIX_PRISM_ELEMENT_CLUSTER_NAME=PHX-POC073
 
 # NKP cluster subnet
 # Do not change it
-export NUTANIX_SUBNET_NAME=aux-1
+export NUTANIX_SUBNET_NAME=secondary
 export DOMAIN=buildit.services
 
 # Prism storage container
@@ -45,5 +45,5 @@ export DOMAIN=buildit.services
 export TIMEOUT=60m
 export NUTANIX_STORAGE_CONTAINER_NAME=SelfServiceContainer
 export REGISTRY_MIRROR_URL=registry.nutanixdemo.com/docker.io
-export IMAGES=/home/nutanix/assets/nkp-v2.16.1/container-images/kommander-image-bundle-v2.16.1.tar,
-export IMAGES+=/home/nutanix/assets/nkp-v2.16.1/container-images/konvoy-image-bundle-v2.16.1.tar
+export IMAGES=/home/ubuntu/assets/nkp-v2.16.1/container-images/kommander-image-bundle-v2.16.1.tar,
+export IMAGES+=/home/ubuntu/assets/nkp-v2.16.1/container-images/konvoy-image-bundle-v2.16.1.tar
