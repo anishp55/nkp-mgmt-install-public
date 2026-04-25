89befddb18470e67b589bbbf6e47ce54bab6b296
anishp55 <anishs.online.bills@gmail.com>
2026-04-25 14:31:58 -0400

    template out ip, used /Users/anish.patel for home

--- nkp-vars.sh
+++ nkp-vars.sh
@@ -2,8 +2,8 @@
 
 # NKP version to install
 # Do not change it
-export NKP_VERSION=2.16.1
-
+export NKP_VERSION=2.17.1
+export BASE_ADDRESS=10.8.18
 # NKP cluster name.
 # When using NKP Pro/Ultimate, this name is used to generate the license key
 export CLUSTER_NAME=rccl-example
@@ -14,7 +14,7 @@ export CLUSTER_NAME=rccl-example
 # Keep the password enclosed between single quotes - Ex: 'password'
 #set as env var through GHA secrets
 # Prism Central IP address - Ex: 10.38.30.7
-export NUTANIX_ENDPOINT=10.8.50.7
+export NUTANIX_ENDPOINT=${BASE_ADDRESS}.7
 
 # Prism Central port (default: 9440)
 # Do not change it
@@ -22,18 +22,18 @@ export NUTANIX_PORT=9440
 
 # Kubernetes VIP. Must be in the same subnet as the VMs
 # Check the table to find your IP
-export CONTROL_PLANE_ENDPOINT_IP=10.8.50.140
+export CONTROL_PLANE_ENDPOINT_IP=${BASE_ADDRESS}.134
 
 # Load balancer IP range. Format: <first_ip>-<last_ip>
 # Check the table to find your IP
-export LB_IP_RANGE=10.8.50.141-10.42.73.144
+export LB_IP_RANGE=${BASE_ADDRESS}.135-${BASE_ADDRESS}.136
 
 # NKP Rocky image name
 # Do not change it
-export NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME=nkp-ubuntu-22.04-release-cis-1.33.5-20251108010758.qcow2
+export NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME=nkp-rocky-9.7-release-cis-1.34.3-20260316170119.qcow2
 
 # Prism Element cluster name - Ex: PHX-POC207
-export NUTANIX_PRISM_ELEMENT_CLUSTER_NAME=RNO-SPOC007-3
+export NUTANIX_PRISM_ELEMENT_CLUSTER_NAME=RNO-POC007
 
 # NKP cluster subnet
 # Do not change it
@@ -45,5 +45,6 @@ export DOMAIN=buildit.services
 export TIMEOUT=60m
 export NUTANIX_STORAGE_CONTAINER_NAME=SelfServiceContainer
 export REGISTRY_MIRROR_URL=registry.nutanixdemo.com/docker.io
-export IMAGES=/home/ubuntu/assets/nkp-v2.16.1/container-images/kommander-image-bundle-v2.16.1.tar,
-export IMAGES+=/home/ubuntu/assets/nkp-v2.16.1/container-images/konvoy-image-bundle-v2.16.1.tar
+IMAGES=${HOME}/assets/nkp-v${NKP_VERSION}/container-images/kommander-image-bundle-v${NKP_VERSION}.tar,
+IMAGES+=${HOME}/assets/nkp-v${NKP_VERSION}/container-images/konvoy-image-bundle-v${NKP_VERSION}.tar
+export IMAGES
